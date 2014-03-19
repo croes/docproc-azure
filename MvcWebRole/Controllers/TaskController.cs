325fd17e5c504afcddddcc93ab4b4fcb1bed1e52
gcroes <glenncroes@gmail.com>
2014-03-19 16:55:51 +0100

    Added JobDAO and TaskController

---
+++ MvcWebRole/Controllers/TaskController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.WindowsAzure.ServiceRuntime;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.RetryPolicies;
+using Microsoft.WindowsAzure.Storage.Table;
+using MvcWebRole.DataAccessLayer;
+using MvcWebRole.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcWebRole.Controllers
+{
+    public class TaskController : Controller
+    {
+
+        private TaskDAO taskDoa;
+
+        public TaskController()
+        {
+            taskDoa = new TaskDAO();
+        }
+
+        public ActionResult Index(string jobPartitionKey, string jobRowKey)
+        {
+            JobDAO jobDAO = new JobDAO();
+            Job job = jobDAO.FindJob(jobPartitionKey, jobRowKey);
+            ViewBag.Job = job;
+            return View(taskDoa.FindTasksOfJob(job));
+        }
+
+        public ActionResult Delete(string partitionKey, string rowKey)
+        {
+            var task = taskDoa.FindTask(partitionKey, rowKey);
+            return View(task);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string partitionKey, string rowKey)
+        {
+            taskDoa.DeleteTask(partitionKey, rowKey);
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(string partitionKey, string rowKey)
+        {
+            var job = taskDoa.FindTask(partitionKey, rowKey);
+            return View(job);
+        }
+
+        public static Expression<Func<Task, bool>> HasRowKeyPrefix(String prefix)
+        {
+            char lastChar = prefix[prefix.Length - 1];
+            char nextLastChar = (char)((int)lastChar + 1);
+            string nextPrefix = prefix.Substring(0, prefix.Length - 1) + nextLastChar;
+
+            return e => e.RowKey.CompareTo(prefix) >= 0 && e.RowKey.CompareTo(nextPrefix) < 0;
+        }
+    }
+}
--- WorkerRole/WorkerRole.cs
+++ WorkerRole/WorkerRole.cs
@@ -16,11 +16,14 @@ namespace WorkerRole
         public override void Run()
         {
             // This is a sample worker implementation. Replace with your logic.
-            Trace.TraceInformation("WorkerRole entry point called", "Information");
+            Trace.TraceInformation("CsvToDataWorkerRole entry point called");
 
             while (true)
             {
-                Thread.Sleep(10000);
+
+                var contents = File.ReadAllText(filename).Split('\n');
+                var csv = from line in contents
+                          select line.Split(',').ToArray();
                 Trace.TraceInformation("Working", "Information");
             }
         }
@@ -34,6 +37,33 @@ namespace WorkerRole
             // see the MSDN topic at http://go.microsoft.com/fwlink/?LinkId=166357.
 
             return base.OnStart();
+        }
+
+        /// <summary>
+        /// Starts the specified number of dequeue tasks.
+        /// </summary>
+        /// <param name="threadCount">The number of dequeue tasks.</param>
+        public void StartListener(int threadCount)
+        {
+            Guard.ArgumentNotZeroOrNegativeValue(threadCount, "threadCount");
+
+            // The collection of dequeue tasks needs to be reset on each call to this method.
+            if (this.dequeueTasks.IsAddingCompleted)
+            {
+                this.dequeueTasks = new BlockingCollection<Task>(this.dequeueTaskList);
+            }
+
+            for (int i = 0; i < threadCount; i++)
+            {
+                CancellationToken cancellationToken = this.cancellationSignal.Token;
+                CloudQueueListenerDequeueTaskState<T> workerState = new CloudQueueListenerDequeueTaskState<T>(Subscriptions, cancellationToken, this.queueLocation, this.queueStorage);
+
+                // Start a new dequeue task and register it in the collection of tasks internally managed by this component.
+                this.dequeueTasks.Add(Task.Factory.StartNew(DequeueTaskMain, workerState, cancellationToken, TaskCreationOptions.LongRunning, TaskScheduler.Default));
+            }
+
+            // Mark this collection as not accepting any more additions.
+            this.dequeueTasks.CompleteAdding();
         }
     }
 }
